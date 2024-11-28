f9d9e8e8d0c26a05f02ec3fb7c3e8d976bbb8228
imbytecat <imbytecat@gmail.com>
2024-11-28 23:28:37 +0800

    1

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import type { PinoLogger } from 'hono-pino'
 import { logger } from '@/middlewares/logger'
 import { OpenAPIHono } from '@hono/zod-openapi'
-import { notFound, onError } from 'stoker/middlewares'
+import { notFound, onError, serveEmojiFavicon } from 'stoker/middlewares'
 
 type AppBindings = {
   Variables: {
@@ -10,13 +10,15 @@ type AppBindings = {
 }
 
 const app = new OpenAPIHono<AppBindings>()
+
 app.use(logger())
+app.use(serveEmojiFavicon('🔥'))
+
+app.notFound(notFound)
+app.onError(onError)
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
-
-app.notFound(notFound)
-app.onError(onError)
 
 export default app
